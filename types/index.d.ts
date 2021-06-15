33aaed5902213358f105b104fef8ef316d20f5e5
Hubert Walczak <hubertwalczak8@gmail.com>
2021-06-15 11:36:26 +0200

    Bumped depedency of SW

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,9 +4,9 @@ import http from 'http';
 import { SimpleWebSocket } from 'simple-websockets';
 declare type ListenerCallback = (socket: SimpleWebSocket, request: http.IncomingMessage) => void;
 declare class SimpleWebSocketServer extends ws.Server {
-    connectionListeners: ListenerCallback[];
-    constructor(options?: ws.ServerOptions, callback?: () => void);
-    onConnection(listener: ListenerCallback): void;
-    send(eventName: string, ...values: any[]): void;
+	connectionListeners: ListenerCallback[];
+	constructor(options?: ws.ServerOptions, callback?: () => void);
+	onConnection(listener: ListenerCallback): void;
+	send(eventName: string, ...values: any[]): void;
 }
 export { SimpleWebSocketServer };
