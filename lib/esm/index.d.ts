fa3507e124fd1f2a0ea659c21a2544cba3aadf96
Hubert Walczak <hubertwalczak8@gmail.com>
2023-10-12 09:38:35 +0200

    up deps

---
+++ lib/esm/index.d.ts
@@ -0,0 +1,14 @@
+/// <reference types="node" />
+import ws from 'ws';
+import http from 'http';
+import { SimpleWebSocket, convertEventToMessage, getEnvironment, convertMessageToEvent } from 'simple-websockets';
+declare type ListenerCallback = (socket: SimpleWebSocket, request: http.IncomingMessage) => void;
+declare class SimpleWebSocketServer extends ws.Server {
+	connectionListeners: ListenerCallback[];
+	constructor(options?: ws.ServerOptions, callback?: () => void);
+	onConnection(listener: ListenerCallback): void;
+	send(eventName: string, ...values: any[]): void;
+}
+export { SimpleWebSocketServer };
+export { getEnvironment, convertMessageToEvent, convertEventToMessage };
+export { SimpleWebSocket };
